4a0c258253e8c314207b334152ebfef425fe81dd
João Neves <contatojotaico@gmail.com>
2024-05-13 11:25:13 -0400

    values.js adicionado

---
+++ Mapas/values.js
@@ -0,0 +1,10 @@
+let mapa = new Map();
+mapa.set('chave1', 'valor1');
+mapa.set('chave2', 'valor2');
+
+let iterator = mapa.values();
+for (let valor of iterator) {
+    for (let valor of iterator) {
+        console.log(valor);
+    }
+}
