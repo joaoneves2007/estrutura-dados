e542a0c09be56679b74727e9f186803f70a764ca
João Neves <contatojotaico@gmail.com>
2024-05-13 10:42:59 -0400

    values.js adicionado

---
+++ Conjuntos/values.js
@@ -0,0 +1,5 @@
+let conjunto = new Set([1, 2, 3]);
+let iterador = conjunto.values();
+conjunto.log(iterador.next().value);
+conjunto.log(iterador.next().value);
+conjunto.log(iterador.next().value);
